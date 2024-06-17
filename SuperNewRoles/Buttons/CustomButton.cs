4630e73d49b4fbf5e37156a2dbeb0ead01784b84
zrotl <89401398+zrotl@users.noreply.github.com>
2024-06-17 23:16:46 +0900

    Change CheckHasButton serially

--- SuperNewRoles/Buttons/CustomButton.cs
+++ SuperNewRoles/Buttons/CustomButton.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -111,7 +110,7 @@ public class CustomButton
 
         if (isAlive != isAliveCache || role != roleCache)
         {
-            Parallel.ForEach(buttons, btn => btn.CheckHasButton(isAlive, role));
+            foreach(CustomButton btn in buttons) btn.CheckHasButton(isAlive, role);
             isAliveCache = isAlive;
             roleCache = role;
         }
